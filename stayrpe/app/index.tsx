06efe037bdee1aec05a7f8357c821c1ad7c76017
LauraDev <lauralodeirodev@gmail.com>
2025-04-13 18:13:48 +0200

    Initial commit

---
+++ stayrpe/app/index.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+export default function Login(){
+    return (
+        <View style={styles.container}>
+            <Text>Login</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff"
+    }
+})

20aa2d42078ea2cf2ee7d910c21472495f572bcf
LauraDev <lauralodeirodev@gmail.com>
2025-05-25 17:41:47 +0200

    Backend implementation of user onboarding flow pt.2

--- demo/src/main/java/com/example/demo/dto/UserProfileResponse.java
+++ demo/src/main/java/com/example/demo/dto/UserProfileResponse.java
@@ -17,22 +17,3 @@ public class UserProfileResponse {
     private String experienceLevel;
     private boolean onboardingComplete;
 }
-// UserProfileResponse.java
-package com.example.demo.dto;
-
-import lombok.Data;
-import lombok.Builder;
-
-@Data
-@Builder
-public class UserProfileResponse {
-    private String username;
-    private String firstName;
-    private String lastName;
-    private Integer age;
-    private Integer height;
-    private Integer weight;
-    private String fitnessGoal;
-    private String experienceLevel;
-    private boolean onboardingComplete;
-}
